948d2069e43115bc92636fe5212237623bc3a712
Papa Amadou Mandiaye NDIAYE <amadou.mandiaye.ndiaye@gmail.com>
2025-03-16 11:06:06 +0000

    MAS de routes pour Railway

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/', function () {
+    return 'Laravel fonctionne sur Railway !';
+});
 
 
 Route::prefix('/yonnee')->controller(App\Http\Controllers\AuthController::class)->group(function(){ 
